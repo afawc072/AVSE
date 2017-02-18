a8680d4a484a602c104c9a6e96e86e833096f8e1
Sbrau <simonbrault@live.ca>
2017-02-18 12:53:40 -0500

    Updated code for midterm

--- arduino/navigation/Go_to_Goal/Go_to_Goal.ino
+++ arduino/navigation/Go_to_Goal/Go_to_Goal.ino
@@ -1,21 +1,20 @@
 /*
+CURRENT BUG: Robot has trouble with straight lines or anyhing that involves going in the negative Y axis
+
 TODO:
       IMPLEMENT SWEEP CAMERA SERVO AT START
-      TEST PROGRAM
       IMPLEMENT COMMUNICATION WITH RASPBRRY PI
-
-      PID potential info
-      https://w3.cs.jmu.edu/spragunr/CS354/handouts/pid.pdf
+      
 */
 
 #include <Servo.h>
 #include <math.h> 
 
 //ODOMETRY VARIABLES
-int leftCount = 0;
+int leftCount = 0;  
 int leftCountOld = 0;
 int leftCountNew = 0; //Total left ticks
-int rightCount = 0;
+int rightCount = 0; 
 int rightCountOld = 0;
 int rightCountNew = 0; //Total right ticks
 
@@ -29,16 +28,23 @@ byte irRight; //Value of right wheel encoder
 //CONSTANTS
 const float pi = 3.1415; //Pi
 const float wheelTicks = 64; //Total ticks per wheel rotation
-const float wheelRadius = 2.125; //DOUBLE CHECK, in cm
-const float wheelBase = 21; //DOUBLE CHECK, in cm
+const float wheelRadius = 2.7; //In cm
+const float wheelBase = 19.05; //In cm
 
-//For wheel velocity equations
+//Translational Velocity
 double Vo = 0;
 
 //LOCATION VARIABLES
 float distanceLeft = 0; //Distance travelled by left wheel
 float distanceRight = 0; //Distance travelled by right wheel
 float distanceCenter = 0; //Distance travelled by center of robot
+
+float oldDistanceLeft = 0;
+float oldDistanceRight = 0;
+float oldDistanceCenter = 0;
+float deltaDistanceLeft = 0;
+float deltaDistanceRight = 0;
+float deltaDistanceCenter = 0;
 
 float positionX = 0; //X position of the robot
 float positionXOld = 0;
@@ -51,7 +57,7 @@ float headingOld = 0;
 float dX; //X difference
 float dY; //Y difference
 
-float w;
+float w; //Angular Velocity
 
 float goalX; //Temporary goal X position to reach
 float goalY; //Temporary goal Y position to reach
@@ -65,14 +71,18 @@ float errorD=0; //D error for the PID
 float errorHeading=0;
 float errorPrevious=0; //Extra error for PID calculations
 
-float Kp = 10; //P tuning parameter
-float Ki = 1/10000; //I tuning parameter
+float Kp = 50; //P tuning parameter
+float Ki = 1/1000; //I tuning parameter
 float Kd = 0.1; //D tuning parameter
 
 float velocityLeft;
 float velocityRight;
+float mappedVelocityLeft;
+float mappedVelocityRight;
 
-boolean moving = false;
+boolean moving = false; //To be used with navigation system
+
+int tempCount = 0;
 
 Servo leftServo; //Servo motor to control the left wheel
 Servo rightServo; //Servo motor to control the right wheel
@@ -80,6 +90,8 @@ Servo cameraServo; //Servo motor to control the camera
 
 unsigned long t1 = 0;
 unsigned long t2 = 0;
+
+int test = 2000;
 
 void setup() 
 {
@@ -94,8 +106,8 @@ void setup()
     cameraServo.attach(9); //Attach camera servo
 
     //Setup interrupts for encoders, to be called on change
-    attachInterrupt(0, updateRightTick, CHANGE);
-    attachInterrupt(1, updateLeftTick, CHANGE);
+    attachInterrupt(0, updateLeftTick, CHANGE);
+    attachInterrupt(1, updateRightTick, CHANGE);
     
     //CALL FUNCTION FOR SWEEP TO FIND TAG
     //findFirstTag();
@@ -104,71 +116,29 @@ void setup()
     //getInitialPosition();
     //moving = true;
     //TEMPORARY VALUES USED FOR TESTING
-    goalX = 10; //X value for testing
-    goalY= 30; //Y value for testing
+    goalX = 50; //X value for testing
+    goalY = 50; //Y value for testing
     
 
     moving = true; //To be changed after integration with Pi to be modified after we get the intial position.
 }
 
+//Main loop, to be run at all time
 void loop() 
-{
+{  
     //Directions are inverted on physical robot hence the change in the value
     
-    //timeBefore = millis(); //Get time at start of loop
+      t2 = millis(); //Get time at start of loop
 
-   
-//    goToGoal(); //Call the go to goal funcion
-//    convertVelocityToMS(); //Call to convert velocity to a value usable by servo motors
-//
-//    destinationReached();
-
-      t2 = millis();
-
-      //wait 0.1 sec
+      //wait 0.1 sec for to let tick count update
       if(t2-t1 >= 100)
       {
         t1 = t2;
-        setupGoToGoal();
-        
-    Serial.print("pX: ");
-    Serial.print(positionX);
-    Serial.print(", pY: ");
-    Serial.print(positionY);
-    Serial.print(", heading: ");
-    Serial.print(heading);
-    Serial.print(", w:");
-    Serial.println(w); 
-
-//    Serial.print("errorP: ");
-//    Serial.print(errorP);
-//    Serial.print(", errorI: ");
-//    Serial.print(errorI);
-//    Serial.print(", errorD: ");
-//    Serial.print(errorD);
-//    Serial.print(", w:");
-//    Serial.println(w); 
-
-//    Serial.print("dX: ");
-//    Serial.print(dX);
-//    Serial.print(", dY: ");
-//    Serial.print(dY);
-//    Serial.print(", w:");
-//    Serial.print(w);
-//    Serial.print(", vLeft: ");
-//    Serial.print(velocityLeft);
-//    Serial.print(": vRight:");
-//    Serial.println(velocityRight); 
-
-//      Serial.print("lTicks: ");
-//      Serial.print(leftCountNew);
-//      Serial.print(", rTicks: ");
-//      Serial.println(rightCountNew);
+        setupGoToGoal(); //Update position and error to goal
       }
       else
       {
-        getPosition();
-        
+        getPosition();  //Update position
       }
 }
 
@@ -178,6 +148,8 @@ void setupGoToGoal()
   velocityToPWM();
 }
 
+
+
 void goToGoal()
 { 
   getPosition(); //Get position from encoders
@@ -185,25 +157,20 @@ void goToGoal()
   dX = goalX - positionX; //Difference in X position
   dY = goalY - positionY; //Difference in Y position
   
-  goalHeading = atan2(dY,dX); //Heading required to reach the goal
+  goalHeading = atan(dY / dX); //Heading required to reach the goal
   errorHeading = goalHeading - heading; //Error with current heading
   errorHeading = atan2(sin(errorHeading),cos(errorHeading)); 
   
   //With time
   timeCurrent = micros();
 
-  /* Serial.print("Tcurrent: ");
-  Serial.print(timeCurrent);
-  Serial.print(", Told");
-  Serial.println(timeOld); */
-  
   dt = timeCurrent - timeOld; //Time difference since last execution
-  //dt = dt / 1000; //Convert to seconds
+  dt = dt / 1000; //Convert to seconds
 
-  //Vo = distanceCenter / dt; //Calculate current speed of robot in m/s
-  float dX2 = pow(dX, 2);
-  float dY2 = pow(dY, 2);
-  Vo = sqrt(dY2 + dX2);
+  float X2 = pow(dX, 2); //dX^2
+  float Y2 = pow(dY, 2); //dY^2
+  Vo = 2 * sqrt(Y2 + X2); //Translational velocity, currently bassed on distance to goal
+
   
   errorP = errorHeading; //Proportional error
   errorI = errorI + (errorHeading * dt); //Integral error
@@ -212,39 +179,38 @@ void goToGoal()
   errorPrevious = errorHeading; //Store previous error
   timeOld = timeCurrent; //Reset time
   
- //w = (Kp * errorP) + (Ki * errorI) + (Kd * errorD); //Result of PID
-   w = (Kp * errorP); //P controller only
+  w = (Kp * errorP) + (Ki * errorI) + (Kd * errorD); //Result of PID
   
   velocityLeft = ((2*Vo - w*wheelBase) / (2*wheelRadius)); //Calculated velocity of left wheel
   velocityRight = ((2*Vo + w*wheelBase) / (2*wheelRadius)); //Calculated velocity of right wheel
+  
 }
 
+//Get the current x,y position and heading of the robot
 void getPosition()
 {
-  leftCount = leftCountNew - leftCountOld; //Difference in tick count since last call for left wheel
-  rightCount = rightCountNew - rightCountOld; //Difference in tick count since last call for right wheel
-  
-  distanceLeft = 2 * pi * leftCount / wheelTicks; //Distance travelled by left wheel
-  distanceRight = 2 * pi * rightCount / wheelTicks; //Distance travelled by right wheel
-  distanceCenter = (distanceLeft + distanceRight) / 2; //Distance travelled by center of robot
-  
-  positionX = positionXOld + distanceCenter * cos(headingOld); //Current X position
-  positionY = positionYOld + distanceCenter * sin(headingOld); //Current Y position
-  heading = headingOld + ((distanceRight - distanceLeft) / wheelBase); //Current heading
-  heading = atan2(sin(heading),cos(heading));
+  distanceLeft = 2 * pi * wheelRadius * (leftCountNew / wheelTicks); //Distance traveled by the left wheel
+  distanceRight = 2 * pi * wheelRadius * (rightCountNew / wheelTicks); //Distance traveled by the right wheel
+  distanceCenter = (distanceLeft + distanceRight) / 2; //Distance traveled by the center of the robot
 
-  //Reset previous tick count
-  leftCountOld = leftCountNew; 
-  rightCountOld = rightCountNew;
+  //Change since last call
+  deltaDistanceLeft = distanceLeft - oldDistanceLeft;
+  deltaDistanceRight = distanceRight - oldDistanceRight;
+  deltaDistanceCenter = distanceCenter - oldDistanceCenter;
+  
+  positionX = positionX + deltaDistanceCenter * cos(heading); //X position of the robot
+  positionY = positionY + deltaDistanceCenter * sin(heading); //Y position of the robot
+  heading = heading + (deltaDistanceRight - deltaDistanceLeft) / wheelBase; //Current heading of the robot
 
-  //Reset previous positions value
-  positionXOld = positionX;
-  positionYOld = positionY;
-  headingOld = heading;
+  //Reseting the values for the next call
+  oldDistanceLeft = distanceLeft;
+  oldDistanceRight = distanceRight;
+  oldDistanceCenter = distanceCenter;
 }
 
 //Convert velocity given by PID to value usable by the servos
-//HIGH CHANCE OF THIS FUNCTION BEING THE PROBLEM
+//leftServo zero point = 1355ms
+//rightServo zero point = 1330ms
 void velocityToPWM()
 {
   //Make sure the values are in a range of 180
@@ -265,29 +231,29 @@ void velocityToPWM()
     velocityRight = -90;
   }
 
-  //Get porper velocity in the wheels
+  //Make velocities between 0 and 180
   velocityLeft = velocityLeft + 90;
-  velocityRight = 90 - velocityRight;
+  velocityRight = velocityRight + 90;
 
-  
-   
+  //Translate value to some usable by the writeMicroseconds() function
+  //Range between value is kept the same around their zero point
+  //Right wheel input for the map() function has been inverted because the motor is mounted the opposite way on the robot
+  mappedVelocityLeft = map(velocityLeft, 0, 180, 1025, 1685); //Keep same range as other wheel
+  mappedVelocityRight = map(velocityRight, 180, 0, 1000, 1660);
+    
   //If the wheel is suppose to go in reverse, make it stop as to not mess with the odometry ticks
-  if(velocityLeft <= 90)
+  if(mappedVelocityLeft <= 1355)
   {
-    velocityLeft = 90;
+    mappedVelocityLeft = 1355;
   }
-  else if (velocityRight >=90)
+  else if (mappedVelocityRight >=1330)
   {
-    velocityRight = 90;
+    mappedVelocityRight = 1330;
   }
-  
-  leftServo.write(velocityLeft);
-  rightServo.write(velocityRight);
 
-    /*Serial.print(", vLeft: ");
-    Serial.print(velocityLeft);
-    Serial.print(": vRight:");
-    Serial.println(velocityRight); */
+  //Write the speed values into the motors to make them rotate appropriatly
+  leftServo.writeMicroseconds(mappedVelocityLeft);
+  rightServo.writeMicroseconds(mappedVelocityRight);
 }
 
 
@@ -311,16 +277,10 @@ void updateRightTick()
 //    }
 //}
 //
-//void stopMotor()
-//{
-//  while(true)
-//  {
-//    leftServo.writeMicroseconds(1500);
-//    rightServo.writeMicroseconds(1500);
-//  }
-//}
+
 
 /*
+ * TO BE IMPLEMENTED
 void getInitialPosition()
 {
   //Use protocol go get initial position from the raspberry pi
@@ -330,7 +290,7 @@ void findFirstTag()
 {
   //Either sweep servo motor until stop command from pi and send camera servo angle
   //OR
-  //Get specific angle from pi to move servo to
+  //Get specific angle from pi to move servo to that angle
 }
 
 */
